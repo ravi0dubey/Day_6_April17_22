ec86253c3a96efb49cf2c20c4580eabc661ce7ac
Ravi Dubey <38419795+ravi0dubey@users.noreply.github.com>
2022-04-18 01:08:47 +0000

    Hurdle 4

--- main.py
+++ main.py
@@ -52,6 +52,69 @@ while not at_goal():
     jump()
         
     
+#Hurdle 4
+  def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+  
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+def jump1():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+    
+def jump2():
+    turn_left()
+    move()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+    #number_of_hurdles = 6
+
+#while(number_of_hurdles > 0):
+#    jump()
+#    number_of_hurdles -=1
+#    if(at_goal()):
+#        number_of_hurdles = 0
+        
+#while at_goal() != True:
+#    jump()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    if wall_in_front():
+        jump2()
+    
+        
+    
+
+
+
+
+
+
 
 
 
